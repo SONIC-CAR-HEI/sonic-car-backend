98201f824956292433a055ddeee75c6748fef3b1
kirisaki_vk <kirisaki.vk@gmail.com>
2024-05-23 20:16:17 +0300

    chore: specification endpoint config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    const openApiConfig = new DocumentBuilder()
+        .setTitle("SONIC Car Dealer API")
+        .setDescription("API specs for the Sonic Car Dealer app")
+        .setVersion("0.6.0")
+        .build();
+
+    const openApiDocument = SwaggerModule.createDocument(app, openApiConfig);
+    SwaggerModule.setup("specs", app, openApiDocument);
+
     await app.listen(3000);
 }
 bootstrap();
