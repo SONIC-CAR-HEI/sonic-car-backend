c790794fd4651b77f70b503b9dacddd6a4b50647
kirisaki_vk <kirisaki.vk@gmail.com>
2024-05-23 20:14:29 +0300

    chore: add config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,17 @@ import { AppService } from "./app.service";
 import { AppController } from "./app.controller";
 import { CarModule } from "./car/car.module";
 import { AppointmentModule } from "./appointment/appointment.module";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
     imports: [CarModule, AppointmentModule],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+        }),
+        CarModule,
+        AppointmentModule,
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
