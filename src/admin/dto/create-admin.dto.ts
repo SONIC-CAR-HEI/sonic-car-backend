22e101a15630fd9c137fe5d4e37f676c8e58e6c1
kirisaki_vk <kirisaki.vk@gmail.com>
2024-05-24 17:46:24 +0300

    feat: admin creation validation

--- src/admin/dto/create-admin.dto.ts
+++ src/admin/dto/create-admin.dto.ts
@@ -1,9 +1,18 @@
-import { IsEmail } from "class-validator";
+import { IsEmail, IsStrongPassword, MinLength } from "class-validator";
 
 export class CreateAdminDto {
+    @MinLength(3)
     firstName: string;
+    @MinLength(3)
     lastName: string;
     @IsEmail()
     email: string;
+    @IsStrongPassword({
+        minLowercase: 1,
+        minNumbers: 1,
+        minUppercase: 0,
+        minSymbols: 0,
+        minLength: 8,
+    })
     password: string;
 }
