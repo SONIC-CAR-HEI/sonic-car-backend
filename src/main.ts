13892a8e4c56a5bcbd9a02fa264e5929dc7fbd53
kirisaki_vk <kirisaki.vk@gmail.com>
2024-06-07 11:07:37 +0300

    chore: use alt cors config

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,18 @@ import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule, { cors: true });
+    const app = await NestFactory.create(AppModule);
+
+    app.enableCors({
+        origin: [
+            "https://sonic-car-backend.vercel.app",
+            "http://localhost:3001",
+        ],
+        methods: "GET, HEAD, PUT, POST, DELETE, OPTIONS, PATCH",
+        credentials: true,
+        allowedHeaders:
+            "Origin, X-Requested-With, Content-Type, Accept, Authentication, Access-control-allow-credentials, Access-control-allow-headers, Access-control-allow-methods, Access-control-allow-origin, User-Agent, Referer, Accept-Encoding, Accept-Language, Access-Control-Request-Headers, Cache-Control, Pragma",
+    });
 
     const openApiConfig = new DocumentBuilder()
         .setTitle("SONIC Car Dealer API")
